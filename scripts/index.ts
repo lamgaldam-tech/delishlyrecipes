24ee1a4e26526bb5e2ad4b9646a4787c19c5e802
younes-alhyan <younesalhyan555@gmail.com>
2026-03-02 12:00:00 +0000

    refactor: unify website and dashboard to share components

--- scripts/index.ts
+++
@@ -1,9 +0,0 @@
-import { fetchRecipes } from "./fetchRecipes.ts";
-import { fetchImages } from "./fetchImages.ts";
-import { buildStaticPages } from "./buildStaticPages.ts";
-
-(async () => {
-  await fetchRecipes();
-  await fetchImages();
-  buildStaticPages();
-})();
--- src/types/page.types.ts
+++
@@ -1,6 +0,0 @@
-export interface Page {
-  html: string;
-  script: string;
-  header: string;
-  body: string;
-}
