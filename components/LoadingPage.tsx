a031442af22a1adc567bb510b4145e9fd7626324
younes-alhyan <younesalhyan555@gmail.com>
2026-03-08 18:04:33 +0000

    feat(components): add LoadingPage component

---
+++ components/LoadingPage.tsx
@@ -0,0 +1,7 @@
+import { Loader2 } from "lucide-react";
+
+export const LoadingPage = () => (
+  <div className="min-h-screen w-full bg-primary text-text-primary flex items-center justify-center px-4">
+    <Loader2 className="animate-spin" size={24} />
+  </div>
+);
